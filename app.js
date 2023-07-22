6bae726bbbe81c94549278c39717ca33e08274a4
SliskiPlumek <hsliwinski49@gmail.com>
2023-07-22 20:22:01 +0200

    Deployment Setting

--- app.js
+++ app.js
@@ -21,11 +21,12 @@ const resolvers = require("./graphql/resolvers");
 const typeDefs = require("./graphql/typeDefs");
 
 const app = express();
+const private_key = process.env.SERVICE_ACCOUNT_PRIVATE_KEY
 const serviceAccount = {
   "type": process.env.SERVICE_ACCOUNT_TYPE,
   "project_id": process.env.SERVICE_ACCOUNT_PROJECT_ID,
   "private_key_id": process.env.SERVICE_ACCOUNT_PRIVATE_KEY_ID,
-  "private_key": process.env.SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  "private_key": private_key.replace(/\\n/g, "\n"),
   "client_email": process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
   "client_id": process.env.SERVICE_ACCOUNT_CLIENT_ID,
   "auth_uri": process.env.SERVICE_ACCOUNT_AUTH_URI,
