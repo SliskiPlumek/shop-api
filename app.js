f309aa6b973ed8ce904be569f8d9f02a08d68cb5
SliskiPlumek <hsliwinski49@gmail.com>
2023-07-22 20:18:09 +0200

    Deployment Setting

--- app.js
+++ app.js
@@ -21,7 +21,20 @@ const resolvers = require("./graphql/resolvers");
 const typeDefs = require("./graphql/typeDefs");
 
 const app = express();
-const serviceAccount = process.env.SERVICE_ACCOUNT;
+const serviceAccount = {
+  "type": process.env.SERVICE_ACCOUNT_TYPE,
+  "project_id": process.env.SERVICE_ACCOUNT_PROJECT_ID,
+  "private_key_id": process.env.SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+  "private_key": process.env.SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  "client_email": process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
+  "client_id": process.env.SERVICE_ACCOUNT_CLIENT_ID,
+  "auth_uri": process.env.SERVICE_ACCOUNT_AUTH_URI,
+  "token_uri": process.env.SERVICE_ACCOUNT_TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.env.SERVICE_ACCOUNT_CLIENT_X509_CERT_URL,
+  "universe_domain": process.env.SERVICE_ACCOUNT_UNIVERSE_DOMAIN
+};
+
 
 const parser = bodyParser.json();
 const accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), {flags: 'a'})
